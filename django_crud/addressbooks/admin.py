5adce91b1a06d21d96f8a1be7bb890f41ee7e064
Anthony Addae <maaddae.co@gmail.com>
2022-12-27 02:26:38 +0000

    Modify address book models

--- django_crud/addressbooks/admin.py
+++ django_crud/addressbooks/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin  # noqa F401
 
-# Register your models here.
+from .models import Contact, Email, Group, PhoneNumber
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PhoneNumber)
+class PhoneNumberAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Email)
+class EmailAdmin(admin.ModelAdmin):
+    pass
--- django_crud/addressbooks/models.py
+++ django_crud/addressbooks/models.py
@@ -1,3 +1,55 @@
-from django.db import models  # noqa F401
+from django.conf import settings
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Group(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Email(models.Model):
+    """Add extra fields like slug or type to describe or tag email eg. official or personal etc"""
+
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    class Meta:
+        unique_together = ["contact", "email"]
+
+    def __str__(self):
+        return self.email
+
+
+class PhoneNumber(models.Model):
+    """Add extra fields like slug or type to describe or tag phone number eg. office or home etc"""
+
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
+
+    class Meta:
+        unique_together = ["contact", "phone_number"]
+
+    def __str__(self):
+        return self.phone_number
