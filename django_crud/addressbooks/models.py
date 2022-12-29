fe4ffba002257eb1348773e8a1b3911f8e316c82
Anthony Addae <maaddae.co@gmail.com>
2022-12-29 13:04:41 +0000

    Modify address book models

--- django_crud/addressbooks/models.py
+++ django_crud/addressbooks/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Group(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
